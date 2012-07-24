c7a08c74b55e14a0be7917dc942e768a5802c14c
yang peng <msneeq@gmail.com>
2012-07-24 19:54:39 +0800

    merge

--- README.md
+++
@@ -1,4 +0,0 @@
-yangpeng
-========
-
-my blog
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YangpengMe::Application.config.secret_token = 'a24a361888d32ba594a322b3bcfd366a9a4a814f21c105778a7e05ae7f3a022596b3598f8142844275aca3c431cdd0fb039dcffb41436f4abe956d256e2cffb1'
